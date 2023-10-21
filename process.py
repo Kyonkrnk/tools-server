7b2e21f123a1fa89fd83aa6c121dd9f906d7c642
kyonkrnk <kyontax.sk15@gmail.com>
2023-10-22 00:28:30 +0900

    再起動用ファイル

---
+++ process.py
@@ -0,0 +1,25 @@
+#サーバーがダウン(？)した場合に自動的にプロセスを再起動させるスクリプト
+import time
+import psutil
+import requests
+import subprocess
+
+
+def restart_process():
+    for process in psutil.process_iter():
+        try:
+            cmd_line = process.cmdline()
+            p_id = process.pid
+            if ("python3" in cmd_line) and ("main.py" in cmd_line):
+                subprocess.run(["kill", p_id]) # プロセスをキルする
+                time.sleep(5)
+                subprocess.run(["python3 main.py"])# プロセスを起動する
+        except:
+            pass
+
+
+while True:
+    resp = requests.get("http://localhost:5000/media_dl", timeout=(15.0, 15.0))
+    if resp.status_code != 200:
+        restart_process()
+    time.sleep(20)
