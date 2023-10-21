657be639db321f621c07e8b65a4b60ab82ecde84
kyonkrnk <kyontax.sk15@gmail.com>
2023-10-22 00:39:20 +0900

    再起動用ファイルを追加

--- process.py
+++ process.py
@@ -6,20 +6,28 @@ import subprocess
 
 
 def restart_process():
+    flg = 0
     for process in psutil.process_iter():
         try:
             cmd_line = process.cmdline()
             p_id = process.pid
             if ("python3" in cmd_line) and ("main.py" in cmd_line):
-                subprocess.run(["kill", p_id]) # プロセスをキルする
+                flg += 1
+                subprocess.run(["kill", p_id]) #プロセスをキルする
                 time.sleep(5)
-                subprocess.run(["python3 main.py"])# プロセスを起動する
-        except:
+                subprocess.run(["nohup python3 main.py"]) #プロセスを起動する
+        except: #プロセスへのアクセス権がない場合
             pass
+
+    if flg == 0:
+        subprocess.run(["nohup python3 main.py"]) #プロセスを起動する
 
 
 while True:
-    resp = requests.get("http://localhost:5000/media_dl", timeout=(15.0, 15.0))
-    if resp.status_code != 200:
+    try:
+        resp = requests.get("http://localhost:5000/media_dl", timeout=(15.0, 15.0))
+        if resp.status_code != 200:
+            restart_process()
+    except:
         restart_process()
     time.sleep(20)
