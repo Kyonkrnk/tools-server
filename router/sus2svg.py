1726c4c7950a4b522f7b6da639bfa4e8e1005d1d
kyonkrnk <kyontax.sk15@gmail.com>
2023-12-15 00:43:57 +0900

    譜面生成を追加

---
+++ router/sus2svg.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, File, UploadFile
+from fastapi.responses import FileResponse
+
+import os
+import io
+import uuid
+import pjsekai.scores
+import pjsekai.scores.line
+
+
+router = APIRouter()
+
+@router.get('/sus2svg')
+def sus2svg():
+    path = os.path.join("templates/sus2svg.html")
+    return FileResponse(path) 
+
+@router.post('/sus2svg')
+async def generate(fileInput: UploadFile = File(...)):
+    # データを読み込む
+    data = await fileInput.read()
+    sus_data = io.TextIOWrapper(io.BytesIO(data))
+
+    # 描画する
+    self = pjsekai.scores.Score()
+    self._init_by_lines([pjsekai.scores.line.Line(line) for line in sus_data.readlines()])
+    drawing = pjsekai.scores.Drawing(score=self)
+
+    # 保存する
+    name = str(uuid.uuid4())
+    drawing.svg().saveas(f"svg/{name}.svg")
+    
+    # 送る
+    return FileResponse(f"svg/{name}.svg")
