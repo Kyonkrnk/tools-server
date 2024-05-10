36e1613cb8ac1c701918d12cf3b53aad447e719c
kyonkrnk <kyontax.sk15@gmail.com>
2024-05-10 21:04:40 +0900

    CHART

--- router/chart_dl.py
+++ router/chart_dl.py
@@ -4,13 +4,13 @@ import json
 
 router = APIRouter()
 
-@router.get('/chart_dl/version')
-def chart_dl():
-    return {"App-Version": "0.0.0"}
-
 @router.get('/chart_dl/api/v1/version')
 def chart_dl_version():
-    return {"App-Version": "2.1.1"}
+    return {"App-Version": "0.0.0"}
+
+@router.get('/chart_dl/api/v2/version')
+def chart_dl_version():
+    return {"App-Version": "2.2.0"}
 
 @router.get('/chart_dl/download')
 def chart_dl_download(password: str = Header()):
